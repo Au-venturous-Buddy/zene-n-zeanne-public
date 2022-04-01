c7252ef4246462c703135a712f5371a36d66fa27
KielLagmay <ezekiel.lagmay@yahoo.com>
2022-04-01 14:14:20 +0800

    Update get-window-dimensions.js

--- src/hooks/get-window-dimensions.js
+++ src/hooks/get-window-dimensions.js
@@ -23,7 +23,7 @@ function GetWindowDimensions() {
 }
 
 export default function UseWindowDimensions() {
-  //if(isBrowser) {
+  if(isBrowser) {
     const [windowDimensions, setWindowDimensions] = useState(GetWindowDimensions());
 
     useEffect(() => {
@@ -36,8 +36,7 @@ export default function UseWindowDimensions() {
     }, []);
 
     return windowDimensions;
-  //}
-  /*
+  }
   else {
     const width = 1200;
     const height = 800;
@@ -46,5 +45,4 @@ export default function UseWindowDimensions() {
       height
     };
   }
-  */
 }
