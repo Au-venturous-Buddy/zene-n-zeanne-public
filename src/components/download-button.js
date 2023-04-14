f51c08552ddc574931d531eceb79fe70a30b039b
KielLagmay <ezekiel.lagmay@yahoo.com>
2023-04-14 22:49:24 +0800

    Update download-button.js

--- src/components/download-button.js
+++ src/components/download-button.js
@@ -16,7 +16,7 @@ export default function DownloadButton({downloadLink}) {
           delay={{ show: 250, hide: 250 }}
           overlay={helpTooltip("Download")}
         >
-        <Button disabled={!(contents.metadataItems.childMarkdownRemark.frontmatter.download_link)} style={{fontSize: ResponsiveSize(0.8, "rem", 0.001, 500), backgroundColor: "#01A5FF"}} aria-label="Download" href={downloadLink} target="_blank" rel="noreferrer">
+        <Button disabled={!(downloadLink)} style={{fontSize: ResponsiveSize(0.8, "rem", 0.001, 500), backgroundColor: "#01A5FF"}} aria-label="Download" href={downloadLink} target="_blank" rel="noreferrer">
             <span aria-hidden={true}><BsDownload aria-hidden={true} /></span>
         </Button>
         </OverlayTrigger>
